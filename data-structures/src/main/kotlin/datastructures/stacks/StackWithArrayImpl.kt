7a9588410191a038027a21679a7adefbb865028e
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2023-06-25 12:33:21 -0300

    feat: add stack with array impl

---
+++ data-structures/src/main/kotlin/datastructures/stacks/StackWithArrayImpl.kt
@@ -0,0 +1,39 @@
+package datastructures.stacks
+
+import datastructures.stacks.exceptions.StackOverFlowException
+import datastructures.stacks.exceptions.StackUnderFlowException
+
+class StackWithArrayImpl<E>(size: Int) : Stack<E> {
+
+    private val elements: Array<E> = arrayOfNulls<Any>(size) as Array<E>
+    private var top: Int = -1
+
+    override fun top(): E {
+        if (isEmpty()) {
+            throw StackUnderFlowException("Stack is empty")
+        }
+        return elements[top]
+    }
+
+    override fun pop(): E {
+        if (isEmpty()) {
+            throw StackUnderFlowException("Stack is empty")
+        }
+        return elements[top--]
+    }
+
+    override fun isEmpty(): Boolean {
+        return top == -1
+    }
+
+    override fun isFull(): Boolean {
+        return top == elements.size -1
+    }
+
+    override fun push(element: E) {
+        if (isFull()) {
+            throw StackOverFlowException("Stack is full")
+        }
+        elements[++top] = element
+    }
+}
