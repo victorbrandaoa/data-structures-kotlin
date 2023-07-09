ceb5f8b9b53d6f26a14cb1e51383465ee20d9027
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2023-07-09 12:03:29 -0300

    feat: add new methods to list interface

--- data-structures/src/main/kotlin/datastructures/lists/List.kt
+++ data-structures/src/main/kotlin/datastructures/lists/List.kt
@@ -2,20 +2,36 @@ package datastructures.lists
 
 interface List<E> {
 
-    fun add(element: E)
+    fun addLast(element: E)
 
-    fun add(index: Int, element: E)
+    fun addAtIndex(index: Int, element: E)
+
+    fun addFirst(element: E)
 
     fun set(index: Int, element: E)
 
-    fun remove(index: Int): E
+    fun removeByIndex(index: Int): E
 
-    fun remove(element: E): E
+    fun removeByElement(element: E): E?
+
+    fun removeFirst(): E
+
+    fun removeLast(): E
 
     fun get(index: Int): E
+
+    fun getFirst(): E
+
+    fun getLast(): E
 
     fun indexOf(element: E): Int
 
     fun contains(element: E): Boolean
+
+    fun isEmpty(): Boolean
+
+    fun toArray(): Array<E>
+
+    fun size(): Int
 
 }
